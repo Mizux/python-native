f14d32c758e5128a4c1eb95c239687fac860a98e
Corentin Le Molgat <corentinl@google.com>
2021-11-05 11:22:16 +0100

    CMake: rework python.cmake

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,3 +1,6 @@
+# Use latest UseSWIG module (3.14) and Python3 module (3.18)
+cmake_minimum_required(VERSION 3.18)
+
 # Will need swig
 set(CMAKE_SWIG_FLAGS)
 find_package(SWIG REQUIRED)
@@ -11,12 +14,96 @@ if(UNIX AND NOT APPLE)
   list(APPEND CMAKE_SWIG_FLAGS "-DSWIGWORDSIZE64")
 endif()
 
-# Find Python
-find_package(Python3 COMPONENTS Interpreter Development.Module REQUIRED)
-list(APPEND CMAKE_SWIG_FLAGS "-py3;-DPY3")
+# Find Python 3
+find_package(Python3 REQUIRED COMPONENTS Interpreter Development.Module)
+list(APPEND CMAKE_SWIG_FLAGS "-py3" "-DPY3")
 
-# Needed by python/CMakeLists.txt
+# Find if the python module is available,
+# otherwise install it (PACKAGE_NAME) to the Python3 user install directory.
+# If CMake option FETCH_PYTHON_DEPS is OFF then issue a fatal error instead.
+# e.g
+# search_python_module(
+#   NAME
+#     mypy_protobuf
+#   PACKAGE
+#     mypy-protobuf
+#   NO_VERSION
+# )
+function(search_python_module)
+  set(options NO_VERSION)
+  set(oneValueArgs NAME PACKAGE)
+  set(multiValueArgs "")
+  cmake_parse_arguments(MODULE
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+  )
+  message(STATUS "Searching python module: \"${MODULE_NAME}\"")
+  if(${MODULE_NO_VERSION})
+    execute_process(
+      COMMAND ${Python3_EXECUTABLE} -c "import ${MODULE_NAME}"
+      RESULT_VARIABLE _RESULT
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(MODULE_VERSION "unknown")
+  else()
+    execute_process(
+      COMMAND ${Python3_EXECUTABLE} -c "import ${MODULE_NAME}; print(${MODULE_NAME}.__version__)"
+      RESULT_VARIABLE _RESULT
+      OUTPUT_VARIABLE MODULE_VERSION
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+  endif()
+  if(${_RESULT} STREQUAL "0")
+    message(STATUS "Found python module: \"${MODULE_NAME}\" (found version \"${MODULE_VERSION}\")")
+  else()
+    if(FETCH_PYTHON_DEPS)
+      message(WARNING "Can't find python module: \"${MODULE_NAME}\", install it using pip...")
+      execute_process(
+        COMMAND ${Python3_EXECUTABLE} -m pip install --user ${MODULE_PACKAGE}
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    else()
+      message(FATAL_ERROR "Can't find python module: \"${MODULE_NAME}\", please install it using your system package manager.")
+    endif()
+  endif()
+endfunction()
+
+# Find if a python builtin module is available.
+# e.g
+# search_python_internal_module(
+#   NAME
+#     mypy_protobuf
+# )
+function(search_python_internal_module)
+  set(options "")
+  set(oneValueArgs NAME)
+  set(multiValueArgs "")
+  cmake_parse_arguments(MODULE
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+  )
+  message(STATUS "Searching python module: \"${MODULE_NAME}\"")
+  execute_process(
+    COMMAND ${Python3_EXECUTABLE} -c "import ${MODULE_NAME}"
+    RESULT_VARIABLE _RESULT
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  if(${_RESULT} STREQUAL "0")
+    message(STATUS "Found python internal module: \"${MODULE_NAME}\"")
+  else()
+    message(FATAL_ERROR "Can't find python internal module \"${MODULE_NAME}\", please install it using your system package manager.")
+  endif()
+endfunction()
+
 set(PYTHON_PROJECT pythonnative)
+message(STATUS "Python project: ${PYTHON_PROJECT}")
 
 # Swig wrap all libraries
 foreach(SUBPROJECT IN ITEMS Foo)
@@ -26,30 +113,12 @@ endforeach()
 #######################
 ## Python Packaging  ##
 #######################
-# Find if python module MODULE_NAME is available,
-# if not install it to the Python user install directory.
-function(search_python_module MODULE_NAME)
-  execute_process(
-    COMMAND ${Python3_EXECUTABLE} -c "import ${MODULE_NAME}; print(${MODULE_NAME}.__version__)"
-    RESULT_VARIABLE _RESULT
-    OUTPUT_VARIABLE MODULE_VERSION
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-  if(${_RESULT} STREQUAL "0")
-    message(STATUS "Found python module: ${MODULE_NAME} (found version \"${MODULE_VERSION}\")")
-  else()
-    message(WARNING "Can't find python module \"${MODULE_NAME}\", user install it using pip...")
-    execute_process(
-      COMMAND ${Python3_EXECUTABLE} -m pip install --upgrade --user ${MODULE_NAME}
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
-  endif()
-endfunction()
+set(PYTHON_PATH ${PROJECT_BINARY_DIR}/python/${PYTHON_PROJECT})
+message(STATUS "Python project build path: ${PYTHON_PATH}")
 
-# Look for required python modules
-search_python_module(setuptools)
-search_python_module(wheel)
+#file(MAKE_DIRECTORY python/${PYTHON_PROJECT})
+file(GENERATE OUTPUT ${PYTHON_PATH}/__init__.py CONTENT "__version__ = \"${PROJECT_VERSION}\"\n")
+file(GENERATE OUTPUT ${PYTHON_PATH}/Foo/__init__.py CONTENT "")
 
 # setup.py.in contains cmake variable e.g. @PYTHON_PROJECT@ and
 # generator expression e.g. $<TARGET_FILE_NAME:pyFoo>
@@ -67,50 +136,52 @@ add_custom_command(
   COMMAND ${CMAKE_COMMAND} -E copy ./$<CONFIG>/setup.py setup.py
   WORKING_DIRECTORY python)
 
-add_custom_command(
-  OUTPUT python/${PYTHON_PROJECT}/__init__.py
-  DEPENDS ${PROJECT_SOURCE_DIR}/python/__init__.py.in
-  COMMAND ${CMAKE_COMMAND} -E copy
-    ${PROJECT_SOURCE_DIR}/python/__init__.py.in
-    ${PYTHON_PROJECT}/__init__.py
-  WORKING_DIRECTORY python)
+# Look for python module wheel
+search_python_module(
+  NAME setuptools
+  PACKAGE setuptools)
+search_python_module(
+  NAME wheel
+  PACKAGE wheel)
 
 add_custom_command(
-  OUTPUT python/${PYTHON_PROJECT}/Foo/__init__.py
-  DEPENDS ${PROJECT_SOURCE_DIR}/python/__init__.py.in
-  COMMAND ${CMAKE_COMMAND} -E copy
-    ${PROJECT_SOURCE_DIR}/python/__init__.py.in
-    ${PYTHON_PROJECT}/Foo/__init__.py
-  WORKING_DIRECTORY python)
-
-add_custom_target(python_package ALL
+  OUTPUT python/dist
+  COMMAND ${CMAKE_COMMAND} -E remove_directory dist
+  #COMMAND ${CMAKE_COMMAND} -E make_directory dist
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${PYTHON_PROJECT}/.libs
+  # Don't need to copy static lib on Windows.
+  COMMAND ${CMAKE_COMMAND} -E $<IF:$<STREQUAL:$<TARGET_PROPERTY:Foo,TYPE>,SHARED_LIBRARY>,copy,true>
+  $<$<STREQUAL:$<TARGET_PROPERTY:Foo,TYPE>,SHARED_LIBRARY>:$<TARGET_SONAME_FILE:Foo>>
+  ${PYTHON_PROJECT}/.libs
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pyFoo> ${PYTHON_PROJECT}/Foo
+  #COMMAND ${Python3_EXECUTABLE} setup.py bdist_egg bdist_wheel
+  COMMAND ${Python3_EXECUTABLE} setup.py bdist_wheel
+  MAIN_DEPENDENCY
+    Foo # can't use TARGET alias here
   DEPENDS
     python/setup.py
-    python/${PYTHON_PROJECT}/__init__.py
-    python/${PYTHON_PROJECT}/Foo/__init__.py
-  COMMAND ${CMAKE_COMMAND} -E remove_directory dist
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${PYTHON_PROJECT}/.libs
-  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pyFoo> ${PYTHON_PROJECT}/Foo
-  # Don't need to copy static lib on Windows
-  COMMAND ${CMAKE_COMMAND} -E $<IF:$<BOOL:${UNIX}>,copy,true>
-    $<TARGET_FILE:Foo> ${PYTHON_PROJECT}/.libs
-  COMMAND ${Python3_EXECUTABLE} setup.py bdist_wheel
   BYPRODUCTS
     python/${PYTHON_PROJECT}
     python/build
     python/dist
     python/${PYTHON_PROJECT}.egg-info
+  WORKING_DIRECTORY python
+  COMMAND_EXPAND_LISTS)
+
+# Main Target
+add_custom_target(python_package ALL
+  DEPENDS
+    python/dist
   WORKING_DIRECTORY python)
 
 ###################
 ##  Python Test  ##
 ###################
 if(BUILD_TESTING)
-  # Look for python module virtualenv
-  search_python_module(virtualenv)
+  search_python_internal_module(NAME venv)
   # Testing using a vitual environment
-  set(VENV_EXECUTABLE ${Python3_EXECUTABLE} -m virtualenv)
-  set(VENV_DIR ${PROJECT_BINARY_DIR}/python/venv)
+  set(VENV_EXECUTABLE ${Python3_EXECUTABLE} -m venv)
+  set(VENV_DIR ${CMAKE_CURRENT_BINARY_DIR}/python/venv)
   if(WIN32)
     set(VENV_Python3_EXECUTABLE "${VENV_DIR}\\Scripts\\python.exe")
   else()
@@ -118,27 +189,28 @@ if(BUILD_TESTING)
   endif()
   # make a virtualenv to install our python package in it
   add_custom_command(TARGET python_package POST_BUILD
-    BYPRODUCTS ${VENV_DIR}
-    COMMAND ${VENV_EXECUTABLE} -p ${Python3_EXECUTABLE} ${VENV_DIR}
+    # Clean previous install otherwise pip install may do nothing
+    COMMAND ${CMAKE_COMMAND} -E remove_directory ${VENV_DIR}
+    COMMAND ${VENV_EXECUTABLE} ${VENV_DIR}
     # Must NOT call it in a folder containing the setup.py otherwise pip call it
     # (i.e. "python setup.py bdist") while we want to consume the wheel package
-    COMMAND ${VENV_Python3_EXECUTABLE} -m pip uninstall -y ${PYTHON_PROJECT}
-    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --find-links=${PROJECT_BINARY_DIR}/python/dist ${PYTHON_PROJECT}
+    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PYTHON_PROJECT}
+    BYPRODUCTS ${VENV_DIR}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "Create venv and install ${PYTHON_PROJECT}"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+    VERBATIM)
 
   add_custom_command(TARGET python_package POST_BUILD
-    BYPRODUCTS python/test/test.py
-    DEPENDS ${PROJECT_SOURCE_DIR}/python/test.py
-    COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/python/test.py test/test.py
+    DEPENDS python/test.py
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/python/test.py ${VENV_DIR}/test.py
+    BYPRODUCTS ${VENV_DIR}/test.py
+    WORKING_DIRECTORY python
     COMMENT "Copying test.py"
-    WORKING_DIRECTORY python)
+    VERBATIM)
 
   # run the tests within the virtualenv
-  add_test(
-    NAME python_test
-    COMMAND ${VENV_Python3_EXECUTABLE} python/test/test.py
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+  add_test(NAME python_test
+    COMMAND ${VENV_Python3_EXECUTABLE} ${VENV_DIR}/test.py)
 endif()
 
 # add_python_example()
